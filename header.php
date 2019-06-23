d2ddd23a7e1eb14db01b0c279a8e22a4ed21f931
MilenaFilippova <33898241+MilenaFilippova@users.noreply.github.com>
2019-06-23 16:48:46 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Авторизация</title>
+	<style type="text/css">
+		.errors
+		{
+			color:red;
+			font-size 200%;
+		}
+	</style>
+</head>
+
+<body>
+		<h1>Авторизация</h1>
+</body>
+<html> 
